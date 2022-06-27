3cbab5be05c2ba15b382c9f7a4e9ca56ab97fe66
Giciarz <106958882+Giciarz@users.noreply.github.com>
2022-06-27 15:12:35 +0200

    update camersky.py

--- camersky.py
+++ camersky.py
@@ -1,28 +1,61 @@
 import tkinter as tk
 from tkinter import *
 from picamera import PiCamera
+import os, os.path
 import time
 from PIL import Image
-# Do photo function
+
+dir_path = "Fotki"
+
+# Starting fuction
+
 def JD():
+    
+    # Cout files
+    
+    global count
+    count = 0
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            count += 1
+    print(count)
+    
+    # Make catalog
+    if not os.path.exists(dir_path):
+        try: 
+            os.makedirs("Fotki")
+        except FileExistsError:
+            pass
+    
+    # Make a Photo
+    
     camera = PiCamera()
     camera.resolution = (1280, 720)
     camera.contrast = 10
     time.sleep(2)
-    camera.capture("/home/adam/Desktop/Twoje_Foto.jpg")
+    
+        
+    camera.capture(f"./Fotki/Twoje_Foto{count + 1}.jpg")
     print("Done.")
+    camera.stop_preview()
+    camera.close()
 
 # Show photo function
 def Show_photo():
 
-    img = Image.open("/home/adam/Desktop/Twoje_Foto.jpg")
+    img = Image.open(f"./Fotki/Twoje_Foto{count + 1}.jpg")
     img.show()
 
+# Del function
+
+def DEL():
+    for f in os.listdir(dir_path):
+        os.remove(os.path.join(dir_path,f))
 
 # photo label
 
 root = tk.Tk()
-root.geometry('300x200')
+root.geometry('600x400')
 root.resizable(True, True)
 root.title('Do a photo')
 root.configure(bg="blue")
@@ -58,6 +91,21 @@ show_button = tk.Button(
 show_button.pack(
     ipadx=5,
     ipady=5
+)
+# Delete button
+del_button = tk.Button(
+    root,
+    text='Delete all photos',
+    bg='purple3',
+    activebackground='purple1',
+    borderwidth='12',
+    relief='ridge',
+    command=DEL
+)
+del_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
 )
 
 # quit button
