46ffbadb00724fd140420a1e47760fbf8411c7e1
Giciarz <106958882+Giciarz@users.noreply.github.com>
2022-06-28 13:34:27 +0200

    update

--- camersky_review.py
+++ camersky_review.py
@@ -5,7 +5,6 @@ import tkinter as tk
 from pathlib import Path
 #from PIL import Image
 
-
 #from picamera import PiCamera
 
 DIR_PATH = "Fotki"
@@ -14,20 +13,18 @@ def count_images():
     global count #never use global variable - maybe if you have really, but really need
     count = 0
     for path in Path(DIR_PATH).iterdir(): #use pathlib, not os.path
-        if Path.isfile(os.path.join(DIR_PATH, path)):
+        if Path.isfile(Path.joinpath(DIR_PATH, path)):
             count += 1
 # Starting fuction
 
 def jd():
 
-    # Make catalog
-    if not os.path.exists(DIR_PATH):
+   # Make catalog
+    if not Path.exists(DIR_PATH):
         try:
-            os.makedirs("Fotki") #os.makedirs(DIR_PATH)
-        except FileExistsError: #except FileExistsError as error
-            pass #print(error) - something like this: try: except: pass is very unaceptable situation for evry programmer
-                # you can not create directory but you don't know that so you thing the directory exist.
-                # please print error
+            Path.mkdir("Fotki")
+        except FileExistsError:
+            print(FileExistsError)
 
     # Make a Photo
 
@@ -37,16 +34,8 @@ def jd():
     sleep(2) #from time import sleep
 
 
-    camera.capture(f"./Fotki/Twoje_Foto{count + 1}.jpg") #it will be working on windows/linux? Nope! Because you using
-    # wrong path structure. Correct way is: pathlib.Path().curdir.joinpath(DIR_PATH).joinpath(Twoje_Foto{count+1}.jpg)
-    # To be honest - you don't need count of files in directory - don't use the count variable, instead of this use
-    # current datetime - from datetime import datetime
-    # Twoje_Foto{datetime.datetime.now()}.jpg
-    # OR
-    # Just get the newest picture and get the last number -
-    # a =  method_to_get_latest_picture()
-    # count = a.split("Twoje_Foto")[1]
-    print("Done.") #Done what?
+    camera.capture(f"./Fotki/Twoje_Foto{count + 1}.jpg") 
+    print("Done.")
     camera.stop_preview()
     camera.close()
 
@@ -58,9 +47,9 @@ def show_photo(): #don't start name of function with up letter - only small
 
 # Del function
 
-def delete(): #This function is stupid - I made 100 pictures and I want keep all except one but I lost all pictures. Why?
-    for f in os.listdir(DIR_PATH): #os.pathlib
-        os.remove(os.path.join(DIR_PATH, f)) #os.pathlib. What happen if user can not remove picture?
+def delete_img():
+    for f in Path.iterdir(DIR_PATH):
+        Path.unlink(Path.joinpath(DIR_PATH,f))
 
 # photo label
 
