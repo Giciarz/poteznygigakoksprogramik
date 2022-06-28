7718d2a57bcaf2bbd1965f1b0e21a9815d9ad027
Giciarz <106958882+Giciarz@users.noreply.github.com>
2022-06-28 13:20:15 +0200

    update

--- camersky_review.py
+++ camersky_review.py
@@ -1,12 +1,11 @@
 import os
 
 from time import sleep
-import tkinter as tk
 from pathlib import Path
-#from PIL import Image
+# from PIL import Image
+# from picamera import PiCamera
 
-
-#from picamera import PiCamera
+import tkinter as tk
 
 DIR_PATH = "Fotki"
 
@@ -14,72 +13,52 @@ def count_images():
     global count #never use global variable - maybe if you have really, but really need
     count = 0
     for path in Path(DIR_PATH).iterdir(): #use pathlib, not os.path
-        if Path.isfile(os.path.join(DIR_PATH, path)):
+        if Path.isfile(Path.joinpath(DIR_PATH, path)):
             count += 1
 # Starting fuction
 
 def jd():
 
     # Make catalog
-    if not os.path.exists(DIR_PATH):
+    if not Path.exists(DIR_PATH):
         try:
-            os.makedirs("Fotki") #os.makedirs(DIR_PATH)
-        except FileExistsError: #except FileExistsError as error
-            pass #print(error) - something like this: try: except: pass is very unaceptable situation for evry programmer
-                # you can not create directory but you don't know that so you thing the directory exist.
-                # please print error
+            Path.mkdir("Fotki")
+        except FileExistsError:
+            print(FileExistsError)
 
     # Make a Photo
 
     camera = PiCamera()
     camera.resolution = (1280, 720)
     camera.contrast = 10
-    time.sleep(2) #from time import sleep
+    sleep(2)
 
 
     camera.capture(f"./Fotki/Twoje_Foto{count + 1}.jpg") #it will be working on windows/linux? Nope! Because you using
     # wrong path structure. Correct way is: pathlib.Path().curdir.joinpath(DIR_PATH).joinpath(Twoje_Foto{count+1}.jpg)
-    # To be honest - you don't need count of files in directory - don't use the count variable, instead of this use
-    # current datetime - from datetime import datetime
-    # Twoje_Foto{datetime.datetime.now()}.jpg
-    # OR
-    # Just get the newest picture and get the last number -
-    # a =  method_to_get_latest_picture()
-    # count = a.split("Twoje_Foto")[1]
     print("Done.") #Done what?
     camera.stop_preview()
     camera.close()
 
 # Show photo function
-def Show_photo(): #don't start name of function with up letter - only small
+def show_photo(): #don't start name of function with up letter - only small
 
     img = Image.open(f"./Fotki/Twoje_Foto{count + 1}.jpg") #So...I can show only one picture? The last? Why not all?
     img.show()
 
 # Del function
 
-def DEL(): #This function is stupid - I made 100 pictures and I want keep all except one but I lost all pictures. Why?
+def delete_img(): #This function is stupid - I made 100 pictures and I want keep all except one but I lost all pictures. Why?
     for f in os.listdir(DIR_PATH): #os.pathlib
-        os.remove(os.path.join(DIR_PATH, f)) #os.pathlib. What happen if user can not remove picture?
+        os.remove(os.path.join(DIR_PATH,f)) #os.pathlib. What happen if user can not remove picture?
 
 # photo label
 
 root = tk.Tk()
-
-screen_width = 600
-screen_height = 300
-
-screen_height = root.winfo_screenheight()
-screen_width = root.winfo_screenwidth()
-
-# center_x = int(screen_width/2 - screen_width / 2)
-# center_y = int(screen_height/2 - screen_height / 2)
-
-root.geometry(f'{screen_width}x{screen_height}')
+root.geometry('600x400') #ugh, why? Use winfo_screenwidth and winfo_screenheight instead using hardcoded value. Or is any reason for this?
 root.resizable(True, True)
 root.title('Do a photo')
 root.configure(bg="blue")
-# root.attributes('-alpha',1)
 
 # photo button
 img_button = tk.Button(
@@ -92,8 +71,8 @@ img_button = tk.Button(
     command=jd
 )
 img_button.pack(
-    ipadx=screen_width/35,
-    ipady=screen_height / 35,
+    ipadx=5, #ugh, why not winfo_screenwidth and divide by any value? You create very small button
+    ipady=5, # the same
     expand=True
 )
 
@@ -106,13 +85,12 @@ show_button = tk.Button(
     relief='ridge',
     bg='dark orange',
     activebackground='orange',
-    command=Show_photo
+    command=show_photo
 
 )
 show_button.pack(
-    ipadx=screen_width/35,
-    ipady=screen_height / 35,
-    expand=True
+    ipadx=5, #the same like earlier
+    ipady=5 #the same like earlier
 )
 # Delete button
 del_button = tk.Button(
@@ -122,11 +100,11 @@ del_button = tk.Button(
     activebackground='purple1',
     borderwidth='12',
     relief='ridge',
-    command=DEL
+    command=delete_img
 )
 del_button.pack(
-    ipadx=screen_width / 35,
-    ipady=screen_height / 35,
+    ipadx=5, #the same like earlier
+    ipady=5, #the same like earlier
     expand=True
 )
 
@@ -142,8 +120,8 @@ exit_button = tk.Button(
 )
 
 exit_button.pack(
-    ipadx=screen_width / 35,
-    ipady=screen_height / 35,
+    ipadx=5, #the same like earlier
+    ipady=5, #the same like earlier
     expand=True
 )
 
